81bbeb8ad4fe1f335c10b9e5da97daa5596907b1
PLP Consulting <Moorlock@gmail.com>
2019-03-12 11:41:24 -0700

    improve QueuingFlowableProcessor documentation

--- rsocket-rpc-js/packages/core/src/QueuingFlowableProcessor.js
+++ rsocket-rpc-js/packages/core/src/QueuingFlowableProcessor.js
@@ -8,7 +8,7 @@
 
 /**
  * An asynchronous, pull-based stream of values. Call the
- * {link IPublisher#subscribe} method to generate a call to the subscriber's
+ * {@link IPublisher#subscribe} method to generate a call to the subscriber's
  * {@link onSubscribe} method with a {@link Subscription} object that has two
  * methods: {@link Subscription#cancel} and {@link Subscription#request}.
  *
@@ -21,6 +21,7 @@
  *
  * @see https://github.com/rsocket/rsocket-js/blob/master/packages/rsocket-types/src/ReactiveStreamTypes.js
  * @interface IPartialSubscriber
+ */
 /**
  * A handler for values provided by a Publisher.
  * @see https://github.com/rsocket/rsocket-js/blob/master/packages/rsocket-types/src/ReactiveStreamTypes.js
@@ -41,9 +42,9 @@ import type {
 /**
  * MAX_REQUEST_N (from QueuingFlowableProcessor.js). If no {@link capacity} is
  * passed to the {@link QueuingFlowableProcessor#constructor}, this value will
- * be used as the capacity (the maximum number of items to request from the
- * source Flowable). This effectively signals that you want an unlimited number
- * of values from the source Flowable.
+ * be used as the capacity (the maximum number of items in the backlog queue).
+ * This effectively signals that you want an unlimited number of items from the
+ * source Flowable.
  *
  * @constant
  * @type {number}
@@ -51,6 +52,14 @@ import type {
 const MAX_REQUEST_N = 0x7fffffff; // uint31
 
 /**
+ * A {@link QueuingFlowableProcessor} is an intermediary between a Flowable
+ * producer and a subscriber. To the producer, it behaves as a greedy subscriber
+ * (that is, one that {@link request}s a large number of emissions immediately).
+ * To the subscriber, it behaves as a {@link Flowable}.
+ *
+ * This allows the producer to generate and emit items at its own fast pace, while
+ * the end subscriber requests them at its own slow pace. In between, the
+ * {@link QueuingFlowableProcessor} queues up the backlog of items.
  */
 export default class QueuingFlowableProcessor<T>
   implements IPublisher, ISubscriber, ISubscription {
@@ -65,27 +74,39 @@ export default class QueuingFlowableProcessor<T>
   _error: ?Error;
 
   /**
-   * @param {?number} [capacity] - the maximum number of items to request from the source Flowable (default = {@link MAX_REQUEST_N})
+   * @param {?number} [capacity] - the maximum size of the backlog queue of
+   * items this {@link QueuingFlowableProcessor} will maintain (default =
+   * {@link MAX_REQUEST_N}). If this queue size is exceeded, the
+   * {@link QueuingFlowableProcessor} will begin to drop incoming items from the
+   * producer.
    */
   constructor(capacity?: number) {
     this._once = false;
     this._requested = 0;
+    /** @type {Subscriber<T>} */
     this._actual = null;
+    /** @type {?Error} */
     this._error = null;
     this._done = false;
     this._wip = 0;
     this._cancelled = false;
+    /** @type {number} */
     this._capacity = capacity;
+    /** @type {T[]} */
     this._queue = [];
+    /** @type {function[]} */
     this._transformers = [];
   }
 
   /**
-   * Subscribe to this {@link QueuingFlowableProcessor}.
+   * The subscriber to this {@link QueuingFlowableProcessor} subscribes by calling
+   * this method.
    *
-   * @param {Subscriber<T>} s
+   * @param {Subscriber<T>} s - an object that implements the Subscriber methods
+   *   this {@link QueuingFlowableProcessor} will call to pass along messages
+   *   from the Flowable producer.
    * @throws {Error} if a Subscriber is already subscribed to this
-   *   QueuingFlowableProcessor
+   *   {@link QueuingFlowableProcessor}
    */
   subscribe(s: Subscriber<T>) {
     if (!this._once) {
@@ -99,7 +120,7 @@ export default class QueuingFlowableProcessor<T>
 
   /**
    * When the {@link QueuingFlowableProcessor} gets an {@link onSubscribe} call
-   * from the source {@link Flowable}, it requests that the {@link Flowable}
+   * from the {@link Flowable} producer, it requests that the {@link Flowable}
    * begin sending it items: either {@link capacity} items, if the capacity was
    * set during the call to {@link QueuingFlowableProcessor#constructor}, or
    * {@link MAX_REQUEST_N} items (effectively unlimited).
@@ -115,8 +136,13 @@ export default class QueuingFlowableProcessor<T>
   }
 
   /**
-   * @param {T} t
-   * @throws {Error} if {@link t} is null. Flowables are not permitted to emit null values.
+   * The {@link Flowable} producer calls this method to add an item to the queue
+   * of this {@link QueuingFlowableProcessor} for later emission to its
+   * subscriber.
+   *
+   * @param {T} t - the item to add
+   * @throws {Error} if {@link t} is null. Flowables are not permitted to emit
+   *   null values.
    */
   onNext(t: T) {
     if (t === null) {
@@ -129,7 +155,12 @@ export default class QueuingFlowableProcessor<T>
   }
 
   /**
-   * @param {Error} t
+   * The {@link Flowable} producer calls this method to signal to this
+   * {@link QueuingFlowableProcessor} that the producer has encountered an error.
+   * The {@link QueuingFlowableProcessor} will pass this error on to its
+   * subscriber after it has finished sending the remaining items in its queue.
+   *
+   * @param {Error} t - the specific error
    */
   onError(t: Error) {
     if (t === null) {
@@ -140,19 +171,27 @@ export default class QueuingFlowableProcessor<T>
     this.drain();
   }
 
+  /**
+   * The {@link Flowable} producer calls this method to signal to this
+   * {@link QueuingFlowableProcessor} that the producer has finished emitting
+   * items. The {@link QueuingFlowableProcessor} will pass this message on to its
+   * subscriber after it has finished sending the remaining items in its queue.
+   */
   onComplete() {
     this._done = true;
     this.drain();
   }
 
   /**
-   * Request a certain number of additional items from this
-   * {@link QueuingFlowableProcessor}.
-   * @example <caption>Note that requests are additive:</caption>
-   * request(2);
-   * request(3); // this is equivalent to request(5);
+   * The subscriber to this {@link QueuingFlowableProcessor} calls this method to
+   * request a certain number of additional items.
    *
-   * @param {number} n the number of items to request
+   * @example <caption>Note that requests are additive:</caption>
+   * // this is equivalent to request(5):
+   * myQFP.request(2);
+   * myQFP.request(3);
+   *
+   * @param {number} n - the number of items to request
    * @throws {Error} if {@link n} is zero or negative
    */
   request(n: number) {
@@ -165,9 +204,9 @@ export default class QueuingFlowableProcessor<T>
   }
 
   /**
-   * Call this to indicate that you want no more items from this
-   * {@link QueuingFlowableProcessor} and that it can stop work and release
-   * its resources.
+   * The subscriber to this {@link QueuingFlowableProcessor} calls this method to
+   * indicate that it wants no more items and that the
+   * {@link QueuingFlowableProcessor} can stop work and empty its queue.
    */
   cancel() {
     this._cancelled = true;
@@ -178,14 +217,16 @@ export default class QueuingFlowableProcessor<T>
   }
 
   /**
-   * Add a mapping function that accepts an item from the source Flowable and
-   * modifies it before it is to be emitted from the
-   * {@link QueuingFlowableProcessor}. Note that you can call this multiple
-   * times to add multiple mapping functions, each of which will operate on the
-   * outputs of its predecessor.
+   * Add a mapping function that modifies each emission from the {@link Flowable}
+   * producer before emitting it from the {@link QueuingFlowableProcessor}. Note
+   * that you can call this multiple times to add multiple mapping functions,
+   * each of which will operate on the outputs of its predecessor.
    *
-   * @param {function} transformer a transformation function
-   * @return {QueuingFlowableProcessorQueuingFlowableProcessor} this same processor, modified with the transformation function
+   * @param {function} transformer - a transformation function that accepts an
+   *   item emitted by the {@link Flowable} producer and returns an item
+   *   expected by the subscriber or by the next transformer
+   * @return {QueuingFlowableProcessor} this same processor, modified with the
+   *   transformation function
    */
   map(transformer) {
     this._transformers.push(transformer);
@@ -194,7 +235,10 @@ export default class QueuingFlowableProcessor<T>
 
   /**
    * Drain the existing queue of items that are ready for emission by the
-   * {@link QueuingFlowableProcessor} by emitting them.
+   * {@link QueuingFlowableProcessor} by emitting them to its subscriber. This
+   * is typically called implicitly by other methods of the
+   * {@link QueuingFlowableProcessor}, not explicitly by the producer or
+   * subscriber.
    */
   drain() {
     if (this._actual == null) {
